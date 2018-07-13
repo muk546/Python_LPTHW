755076ad477291e24f02ef4dd9a4ba1c88a9fe10
Mukul <mukul@Mukuls-Desktop.local>
2018-07-13 00:36:28 -0400

    added ex18_semi_improved.py

---
+++ ex18_semi_improved.py
@@ -0,0 +1,13 @@
+def print_four(*args):
+	arg1, arg2, arg3, arg4 = args
+	print "arg1: %r, arg2: %r, arg3: %r, arg4: %r" % (arg1, arg2, arg3, arg4)
+
+
+
+
+	# print the stuff:
+
+
+print "Decode the Meaning:"
+
+print print_four("Link","To","The","Past")
