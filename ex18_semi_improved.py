540ec5705a0be6367447259bdb2cfc12be7f7571
Mukul <mukul@Mukuls-Desktop.local>
2018-07-13 00:43:02 -0400

    added ex18_semi_improved.py

--- ex18_semi_improved.py
+++ ex18_semi_improved.py
@@ -2,6 +2,9 @@ def print_four(*args):
 	arg1, arg2, arg3, arg4 = args
 	print "arg1: %r, arg2: %r, arg3: %r, arg4: %r" % (arg1, arg2, arg3, arg4)
 
+def print_none():
+	print "Half Empty..."
+
 
 
 
@@ -11,3 +14,6 @@ def print_four(*args):
 print "Decode the Meaning:"
 
 print print_four("Link","To","The","Past")
+
+
+print_none() 
